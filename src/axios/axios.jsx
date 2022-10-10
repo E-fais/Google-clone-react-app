6e08253950e4f2e53b68e663e4c40c29d0617c00
Fais Edathil <102040088+E-fais@users.noreply.github.com>
2022-10-11 00:06:10 +0300

    Update axios.jsx

--- src/axios/axios.jsx
+++ src/axios/axios.jsx
@@ -4,6 +4,6 @@ export const instance = axios.create({
 
     headers: {'X-User-Agent': 'desktop',
     'X-Proxy-Location': 'IN',
-    'X-RapidAPI-Key': '34a99fc479mshd4f5dd6442d9f7ep11beb5jsnc9afc1660582',
+    'X-RapidAPI-Key': '',
     'X-RapidAPI-Host': 'google-search3.p.rapidapi.com'}
   });
